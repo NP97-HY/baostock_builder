e55c8487795b3d6a4ff1539b5d589d56b635976b
wang-sd <1216778648@qq.com>
2020-09-01 17:42:28 +0800

    renew

--- builder_baostock/stock_tools/Data_warehouse.py
+++ builder_baostock/stock_tools/Data_warehouse.py
@@ -30,6 +30,21 @@ class data_Warehouse(object):
         """
         get history data for a group stocks
         targetStock : your aim stock,like: ["sh.600300","sh.601987"]
+
+        date 	交易所行情日期 	
+        code 	证券代码 	
+        open 	开盘价 	
+        high 	最高价 	
+        low 	最低价 	
+        close 	收盘价 	
+        preclose 	前收盘价 	见表格下方详细说明
+        volume 	成交量（累计 单位：股） 	
+        amount 	成交额（单位：人民币元） 	
+        adjustflag 	复权状态(1：后复权， 2：前复权，3：不复权） 	
+        turn 	换手率 	[指定交易日的成交量(股)/指定交易日的股票的流通股总股数(股)]*100%
+        tradestatus 	交易状态(1：正常交易 0：停牌） 	
+        pctChg 	涨跌幅（百分比） 	日涨跌幅=[(指定交易日的收盘价-指定交易日前收盘价)/指定交易日前收盘价]*100%
+        isST 	是否ST股，1是，0否 	
         """
         stock_data_list = {}
         for targetStock in stocklist:
@@ -44,6 +59,16 @@ class data_Warehouse(object):
             while (rs.error_code == "0") & rs.next():
                 datalist.append(rs.get_row_data())
             result = pd.DataFrame(datalist, columns=rs.fields)
+            result = result[result["tradestatus"]=="1"]
+            result['open'] = result['open'].astype(float)
+            result['high'] = result['high'].astype(float)
+            result['low'] = result['low'].astype(float)
+            result['close'] = result['close'].astype(float)
+            result['preclose'] = result['preclose'].astype(float)
+            result['volume'] = result['volume'].astype(float)
+            result['amount'] = result['amount'].astype(float)
+            result['turn'] = result['turn'].astype(float)
+            result['pctChg'] = result['pctChg'].astype(float)
             stock_data_list[targetStock] = result
             result = pd.DataFrame(result, columns=rs.fields)
             if save == True:
